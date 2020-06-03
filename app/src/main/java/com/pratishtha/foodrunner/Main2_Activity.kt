9de88313534f126c3bb2a748e5beaf6dffd36f85
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-03 16:20:45 +0530

    Hamburger icon added

--- app/src/main/java/com/pratishtha/foodrunner/Main2_Activity.kt
+++ app/src/main/java/com/pratishtha/foodrunner/Main2_Activity.kt
@@ -2,9 +2,13 @@ package com.pratishtha.foodrunner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.FrameLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
@@ -29,9 +33,58 @@ class Main2_Activity : AppCompatActivity() {
 
         setUpToolbar()
 
+        val actionBarDrawerToggle=ActionBarDrawerToggle(this@Main2_Activity,drawerLayout,R.string.open_drawer,R.string.close_drawer)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+
+            when(it.itemId)
+            {
+                R.id.home->{
+
+                }
+
+                R.id.profile->{
+
+                }
+
+                R.id.favourites->{
+
+                }
+
+                R.id.orderHistory->{
+
+                }
+
+                R.id.faq->{
+
+                }
+
+                R.id.logout->{
+                    
+                }
+
+
+            }
+
+            return@setNavigationItemSelectedListener(true)
+        }
+
     }
     fun setUpToolbar(){
         setSupportActionBar(toolbar)
         supportActionBar?.title="All Restaurants"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
