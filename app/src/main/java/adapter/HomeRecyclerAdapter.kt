15724f9ecc06708d130d0b14e747fa11c455cbad
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-10 03:06:27 +0530

    Favourite Fragment created using Database

--- app/src/main/java/adapter/HomeRecyclerAdapter.kt
+++ app/src/main/java/adapter/HomeRecyclerAdapter.kt
@@ -1,37 +1,51 @@
-
-
 package adapter
 
 import activity.DescriptionActivity
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
+import android.os.Build
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.pratishtha.foodrunner.R
 import com.squareup.picasso.Picasso
+import database.RestaurantDatabase
+import database.RestaurantEntity
+import fragment.HomeFragment
 import model.Restaurant
 
-class HomeRecyclerAdapter(val context:Context,val itemList:ArrayList<Restaurant>) :RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>(){
+class HomeRecyclerAdapter(val context:Context,val itemList:ArrayList<Restaurant>) :RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
 
-    class HomeViewHolder(view: View):RecyclerView.ViewHolder(view){
+    class HomeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-        val txtRestaurantName:TextView=view.findViewById(R.id.txtRestaurantName)
-        val txtPrice:TextView=view.findViewById(R.id.txtPrice)
-        val txtRating:TextView=view.findViewById(R.id.txtRating)
-        val imgRestaurantImage: ImageView =view.findViewById(R.id.imgRestaurantImage)
+        val txtRestaurantName: TextView = view.findViewById(R.id.txtRestaurantName)
+        val txtPrice: TextView = view.findViewById(R.id.txtPrice)
+        val txtRating: TextView = view.findViewById(R.id.txtRating)
+        val imgRestaurantImage: ImageView = view.findViewById(R.id.imgRestaurantImage)
+        val imgFav: ImageButton = view.findViewById(R.id.imgFav)
+        val cardRestaurant: CardView = view.findViewById(R.id.cardRestaurant)
 
-        val rlContent:RelativeLayout=view.findViewById(R.id.rlContent)
+        val rlContent: RelativeLayout = view.findViewById(R.id.rlContent)
 
     }
 
+    var button: Boolean = false
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.recycler_home_single_row,parent,false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_home_single_row, parent, false)
         return HomeViewHolder(view)
     }
 
@@ -39,14 +53,57 @@ class HomeRecyclerAdapter(val context:Context,val itemList:ArrayList<Restaurant>
         return itemList.size
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        val restaurant=itemList[position]
-        holder.txtRestaurantName.text= restaurant.restaurantName
-        holder.txtRating.text=restaurant.restaurantRating
-        holder.txtPrice.text=restaurant.restaurantCost_For_One
+        val restaurant = itemList[position]
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            holder.imgRestaurantImage.clipToOutline = true
+        }
+
+        holder.txtRestaurantName.text = restaurant.restaurantName
+        holder.txtRating.text = restaurant.restaurantRating
+        holder.txtPrice.text = restaurant.restaurantCost_For_One
         // holder.imgRestaurantImage.setImageResource(restaurant.restaurantImage)
 
-        Picasso.get().load(restaurant.restaurantImage).error(R.drawable.default_restaurant_image).into(holder.imgRestaurantImage)
+        Picasso.get().load(restaurant.restaurantImage).error(R.drawable.default_restaurant_image)
+            .into(holder.imgRestaurantImage)
+
+
+        val listOfFavourites = GetAllFavAsyncTask(context).execute().get()
+
+        if (listOfFavourites.isNotEmpty() && listOfFavourites.contains(restaurant.restaurantId.toString())) {
+
+            holder.imgFav.setBackgroundResource(R.drawable.fav)
+        } else {
+            holder.imgFav.setBackgroundResource(R.drawable.fav2)
+        }
+
+        holder.imgFav.setOnClickListener {
+            val restaurantEntity = RestaurantEntity(
+                restaurant.restaurantId.toInt(),
+                restaurant.restaurantName,
+                restaurant.restaurantRating,
+                restaurant.restaurantCost_For_One,
+                restaurant.restaurantImage
+            )
+            if (!DBAsyncTask(context, restaurantEntity, 1).execute().get()) {
+                val async = DBAsyncTask(context, restaurantEntity, 2).execute()
+                val result = async.get()
+                if (result) {
+                    holder.imgFav.setBackgroundResource(R.drawable.fav)
+                }
+                } else {
+                    val async = DBAsyncTask(context, restaurantEntity, 3).execute()
+                    val result = async.get()
+
+                    if (result) {
+                        holder.imgFav.setBackgroundResource(R.drawable.fav2)
+                    }
+                }
+            }
 
         holder.rlContent.setOnClickListener{
             val intent=Intent(context,DescriptionActivity::class.java)
@@ -56,3 +113,53 @@ class HomeRecyclerAdapter(val context:Context,val itemList:ArrayList<Restaurant>
         }
     }
 }
+
+
+        class DBAsyncTask(context: Context, val restaurantEntity: RestaurantEntity, val mode: Int) :
+            AsyncTask<Void, Void, Boolean>() {
+
+            val db = Room.databaseBuilder(context, RestaurantDatabase::class.java, "restaurants-db")
+                .build()
+
+            override fun doInBackground(vararg params: Void?): Boolean {
+                when (mode) {
+
+                    1 -> {
+                        val res: RestaurantEntity? =
+                            db.restaurantDao().getRestaurantById(restaurantEntity.id.toString())
+                        db.close()
+                        return res != null
+                    }
+
+                    2 -> {
+                        db.restaurantDao().insertRestaurant(restaurantEntity)
+                        db.close()
+                        return true
+                    }
+
+                    3 -> {
+                        db.restaurantDao().deleteRestaurant(restaurantEntity)
+                        db.close()
+                        return true
+                    }
+                }
+                return true
+            }
+        }
+
+        class GetAllFavAsyncTask(context: Context) : AsyncTask<Void, Void, List<String>>() {
+            val db = Room.databaseBuilder(context, RestaurantDatabase::class.java, "restaurants-db")
+                .build()
+
+            override fun doInBackground(vararg params: Void?): List<String> {
+                val list = db.restaurantDao().getAllRestaurants()
+                val listOfIds = arrayListOf<String>()
+                for (i in list) {
+                    listOfIds.add(i.id.toString())
+                }
+                return listOfIds
+            }
+        }
+
+
+
--- app/src/main/java/fragment/FavouriteFragment.kt
+++ app/src/main/java/fragment/FavouriteFragment.kt
@@ -1,21 +1,96 @@
 package fragment
 
+import adapter.HomeRecyclerAdapter
+import android.content.Context
+import android.os.AsyncTask
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.pratishtha.foodrunner.R
+import database.RestaurantDatabase
+import database.RestaurantEntity
+import kotlinx.android.synthetic.main.fragment_favourite.*
+import kotlinx.android.synthetic.main.recycler_home_single_row.*
+import model.Restaurant
 
 
 class FavouriteFragment : Fragment() {
+
+    lateinit var recyclerFavourite: RecyclerView
+    lateinit var progressLayout: RelativeLayout
+    lateinit var progressBar: ProgressBar
+    lateinit var rlFav:RelativeLayout
+    lateinit var rlNoFav:RelativeLayout
+    lateinit var recyclerAdapter: HomeRecyclerAdapter
+
+    var dbRestaurantList= arrayListOf<Restaurant>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite, container, false)
-    }
 
+        val view=inflater.inflate(R.layout.fragment_favourite, container, false)
+
+        recyclerFavourite=view.findViewById(R.id.recyclerFavourite)
+        progressLayout=view.findViewById(R.id.progressLayout)
+        progressBar=view.findViewById(R.id.progressBar)
+        rlFav=view.findViewById(R.id.rlFav)
+        rlNoFav=view.findViewById(R.id.rlNoFav)
+
+        progressLayout.visibility=View.VISIBLE
+       setUpRecycler(view)
+
+        return view
+    }
+private fun setUpRecycler(view: View)
+{
+    recyclerFavourite=view.findViewById(R.id.recyclerFavourite)
+    val backgroundList=FavouritesAsync(activity as Context).execute().get()
+    if(backgroundList.isEmpty()){
+        progressLayout.visibility=View.GONE
+        rlFav.visibility=View.GONE
+        rlNoFav.visibility=View.VISIBLE
+    }else{
+        rlFav.visibility=View.VISIBLE
+        rlNoFav.visibility=View.GONE
+        progressLayout.visibility=View.GONE
+
+        for (i in backgroundList){
+            dbRestaurantList.add(
+                Restaurant(
+                    i.id.toString(),
+                    i.name,
+                    i.rating,
+                    i.costForTwo,
+                    i.imageUrl
+                )
+            )
+        }
+        recyclerAdapter= HomeRecyclerAdapter(activity as Context,dbRestaurantList)
+        var layoutManager=LinearLayoutManager(activity)
+        recyclerFavourite.layoutManager=layoutManager
+        recyclerFavourite.itemAnimator=DefaultItemAnimator()
+        recyclerFavourite.adapter=recyclerAdapter
+        recyclerFavourite.setHasFixedSize(true)
+
+    }
+}
+    class FavouritesAsync( context: Context): AsyncTask<Void, Void, List<RestaurantEntity>>() {
+        val db= Room.databaseBuilder(context,RestaurantDatabase::class.java,"restaurants-db").build()
+
+        override fun doInBackground(vararg params: Void?): List<RestaurantEntity> {
+            return db.restaurantDao().getAllRestaurants()
+        }
+    }
 }
